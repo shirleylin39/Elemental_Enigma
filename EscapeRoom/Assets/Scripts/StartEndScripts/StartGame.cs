26aafe7fb86d1f97046267152135d222bb916045
Deepu Sheela <deepusheela@172-26-86-97.eduroam.bath.ac.uk>
2023-12-06 12:28:57 +0000

    Level-2 - adding level 1 and 2 along with start scene

---
+++ EscapeRoom/Assets/Scripts/StartEndScripts/StartGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMouseDown()
+    {
+        // Load the next scene by name
+        SceneManager.LoadScene("level1");
+    }
+}
+
